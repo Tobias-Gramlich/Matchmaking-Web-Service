6b97c882d921aa8fb2ddce2cdd54b77ea505cda6
tgramlich <tobias.gramlich@kardex.com>
2026-02-28 11:51:50 +0100

    Working Connection to User Management

--- src/handlers/UserAuthenticationHandler.js
+++ src/handlers/UserAuthenticationHandler.js
@@ -1,12 +1,23 @@
-const UserAuthenticationHandler = (payload) => {
+const axios = require('axios');
+
+const UserAuthenticationHandler = async (payload) => {
     if (!payload.accessToken) {
         return {"error" : "No Authentication Token"};
     }
 
-    //TODO: Authentication mit Axios
-
-    return {"userId": 1, "userName": "TestName"};
-    
+    //TODO: Other Connection Path
+    try{
+        const response = await axios.post("http://localhost:3001/Users/auth", payload)
+        if(response.data.error){
+            return {"error": response.data.error};
+        }
+        else{
+            return {"userName": response.data.username, "userId": response.data.userId};
+        }
+    }
+    catch{
+        return{"error": "Something went wrong"}
+    }
 };
 
 module.exports = {UserAuthenticationHandler};
